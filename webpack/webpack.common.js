01d2439d45a98ff72186081936d63314ca341f68
alexkoren-dev <alexitkoren@gmail.com>
2021-06-18 21:29:55 +0200

    modify webpack config

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -4,7 +4,7 @@ const webpack = require('webpack')
 const dotenv = require('dotenv')
 
 // call dotenv and it will return an Object with a parsed key 
-const env = dotenv.config().parsed;
+const env = process.env || dotenv.config().parsed;
   
 // reduce it to a nice object, the same as before
 const envKeys = Object.keys(env).reduce((prev, next) => {
